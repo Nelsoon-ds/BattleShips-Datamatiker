8b11a439bc67c412750cfc0ee30e75f26ab03b3d
anph1000 <anph1000@stud.ek.dk>
2025-09-10 10:54:46 +0200

    gamehelper

---
+++ src/GameHelper.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GameHelper {
+
+public GameHelper(){
+    Scanner scanner = new Scanner();
+
+    public String getUserInput(String promt) {
+
+        String userInput = String.valueOf(scanner.nextInt());
+        System.out.println(promt + " ") {
+            return userInput;
+        }
+
+    }
+
+}
+
+}
