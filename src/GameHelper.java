7d488ed010cbbe6f91c285e0b8a13848e56bfd91
David Olsen <148680863+Nelsoon-ds@users.noreply.github.com>
2025-09-12 11:10:40 +0200

    Davids-branch (#1)
    Har lavet en basic løsning til det med array. Tænker at lege lidt med en arraylist istedet senere idag.
    * First Min branch lets play
    
    * 1d Skibs spil

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
 
 public class GameHelper {
+    private final Scanner scanner = new Scanner(System.in);
 
-public GameHelper(){
-    Scanner scanner = new Scanner();
+    public int getUserInput() {
+        int userAnswer;
 
-    public String getUserInput(String promt) {
-
-        String userInput = String.valueOf(scanner.nextInt());
-        System.out.println(promt + " ") {
-            return userInput;
+        while(true) {
+            System.out.println("Vælg koordinater til dit skud Kaptajn. Du kan skyde imellem følgende tal: 0-15");
+            userAnswer = scanner.nextInt();
+            if (userAnswer >= 0 && userAnswer <= 15) {
+                break;
+            } else {
+                System.out.println("Dit svar: " + userAnswer + " er udenfor vores rækkevidde. Prøv igen.");
+            }
+        }
+            System.out.println("Du valgte: " + userAnswer);
+            return userAnswer;
         }
 
+    public void closeScanner() {
+        scanner.close();
     }
-
 }
 
-}
+
